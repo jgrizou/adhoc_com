b80224cc732529568aa9a6e92e5bca1444d45977
Jonathan <jgrizou@jgrizou-flowers.(none)>
2014-09-22 14:47:57 -0500

    remove matlab_tools from adhoc_tools folder

--- code/adhoc_tools/matlab_tools/matlab/get_value_at_index.m
+++
@@ -1,4 +0,0 @@
-function value = get_value_at_index(M, varargin)
-%GET_VALUE_AT_INDEX 
-
-value = M(varargin{:});
--- code/adhoc_tools/matlab_tools/system/tmux/run_in_tmux.m
+++
@@ -1,9 +0,0 @@
-function run_in_tmux(scriptName)
-%RUN_IN_TMUX
-
-
-[s, cmd] = system('tmux list-session', '-echo')
-error('Not implemented yet')
-
-
-
