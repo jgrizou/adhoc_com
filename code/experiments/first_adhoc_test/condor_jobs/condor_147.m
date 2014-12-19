9203ebeefb2a2c0e590be13601e079807ff64e4e
jgrizou <jonathan.grizou@gmail.com>
2014-12-19 14:03:29 +0100

    removed first experiments code

--- code/experiments/first_adhoc_test/condor_jobs/condor_147.m
+++
@@ -1,4 +0,0 @@
-[pathstr, ~, ~] = fileparts(mfilename('fullpath'));
-addpath(genpath(fullfile(pathstr, '..')));
-init
-run_all_exp(147)
--- code/experiments/first_adhoc_test/copy_condor_results.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rsync -avr submit32.cs.utexas.edu:/projects/agents5/grizou/adhoc_com/code/experiments/first_adhoc_test/results/* /home/jgrizou/Dropbox/adhoc_com/code/experiments/first_adhoc_test/results
