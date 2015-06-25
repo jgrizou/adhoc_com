039be84d0dd9291b969e4c5a8dd2aba2276126bb
jgrizou <jonathan.grizou@gmail.com>
2015-06-25 17:05:08 +0200

    added paper

---
+++ paper/copy_figures.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp ../adhoc_com/code/experiments/visuals/plots/* plots/visuals
+cp ../adhoc_com/code/experiments/expIJCAI/plots/* plots/with_noise
+cp ../adhoc_com/code/experiments/expIJCAI_nonoise/plots/* plots/no_noise
